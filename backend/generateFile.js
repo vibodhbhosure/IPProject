3519381388ab7f3c669644d4733ccdef85960573
vibodhbhosure <vibodhbhoure@gmail.com>
2022-10-06 23:10:07 +0530

    cpp execution

--- backend/generateFile.js
+++ backend/generateFile.js
@@ -1,18 +1,21 @@
 const fs = require("fs");
 const path = require("path");
-const dirCodes = path.join(__dirname, "codes");
-const { v4:uuid } = require("uuid"); // require v4 as uuid
+const { v4: uuid } = require("uuid");
 
-if (!fs.existsSync(dirCodes)) { // if the directory does not exist
+const dirCodes = path.join(__dirname, "codes");
+
+if (!fs.existsSync(dirCodes)) {
   fs.mkdirSync(dirCodes, { recursive: true });
 }
 
-const generateFile = async (format, code) => {
-    const jobId = uuid();
-    const fileName = `${jobId}.${format}`;
-    const filePath = path.join(dirCodes, fileName);
-    await fs.writeFileSync(filePath, code);
-    return filePath
+const generateFile = async (format, content) => {
+  const jobId = uuid();
+  const filename = `${jobId}.${format}`;
+  const filepath = path.join(dirCodes, filename);
+  await fs.writeFileSync(filepath, content);
+  return filepath;
 };
 
-module.exports = {generateFile,};
+module.exports = {
+  generateFile,
+};
--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,32 @@
 const express = require("express");
+const { executeCpp } = require("./executeCPP");
+
+
 const { generateFile } = require("./generateFile");
-const bodyParser = require("body-parser");
+
+
+
 const app = express();
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello World!" });
-});
-
 app.post("/run", async (req, res) => {
   const { language = "cpp", code } = req.body;
+
+  console.log(language, "Length:", code.length);
+
   if (code === undefined) {
-    res.status(400).json({ success: false, message: "Code is required!" });
+    return res.status(400).json({ success: false, error: "Empty code body!" });
   }
-  const filePath = await generateFile(language, code);
-  return res.status(200).json({ success: true, filePath });
+  // need to generate a c++ file with content from the request
+  const filepath = await generateFile(language, code);
+  const output = await executeCpp(filepath);
+  res.status(201).json({ filepath, output });
 });
 
-app.listen(5000 || process.env.PORT, () =>
-  console.log("Server Started at port 5000")
-);
+
+app.listen(5000, () => {
+  console.log(`Listening on port 5000!`);
+});
